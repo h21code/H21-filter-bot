4eeb575417f48a4ca346171d04d82cd2d295f67c
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-15 07:49:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2669,8 +2669,8 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        curr_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
-        time_difference = timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
+        cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
+        time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000)))
         remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
         cap = script.NOR_TXT.format(search, message.from_user.mention, total_results, message.chat.title, remaining_seconds)
     if imdb and imdb.get('poster'):
