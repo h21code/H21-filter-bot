78bb58594a9c310cf5d95d2b5c1c1a2eaef6a319
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-15 11:16:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -136,7 +136,6 @@ async def next_page(bot, query):
             ]
         else:
             cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
-
            # btn = [
           #      [
             #        InlineKeyboardButton(
@@ -148,7 +147,6 @@ async def next_page(bot, query):
                   #  ),
                # ]
                 for file in files
-                    
            # ]
     try:
         if settings['auto_delete']:
