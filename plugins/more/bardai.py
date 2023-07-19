4d8c230f7a4fe28bb9903311929e2cc6a34af132
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-19 16:34:57 +0530

    Update bardai.py

--- plugins/more/bardai.py
+++ plugins/more/bardai.py
@@ -16,6 +16,7 @@ def split_long_message(caption, max_length):
     messages.append(caption)
     return messages
 
+
 def result(api_response, user, query):
     try:
         response_data = api_response['choices']
@@ -28,10 +29,10 @@ def result(api_response, user, query):
         # Adding user and query information to the result
         result_str = f"👤 Requested by: {user}\n\n🔎 Query: {query}\n\n" + result_str
 
-        # Limit the caption length to avoid MediaCaptionTooLong error
-        max_caption_length = 1024  # Set the desired maximum length for the caption
-        if len(result_str) > max_caption_length:
-            messages = split_long_message(result_str, max_caption_length)
+        # Limit the message length to avoid MessageTooLong error
+        max_message_length = 4096  # Set the desired maximum length for the message
+        if len(result_str) > max_message_length:
+            messages = split_long_message(result_str, max_message_length)
         else:
             messages = [result_str]
 
@@ -51,10 +52,10 @@ async def reply_info(client, message):
     user = message.from_user.username or message.from_user.first_name
     
     result_messages = result(api_response, user, query)
-    for idx, result_caption in enumerate(result_messages):
-        # Send each part of the caption as a separate message
+    for idx, result_content in enumerate(result_messages):
+        # Send each part of the message as a separate message
         await message.reply_text(
-            caption=result_caption,
+            text=result_content,
             reply_markup=BUTTONS,
             quote=True if idx == 0 else False  # Quote the first message only
         )
