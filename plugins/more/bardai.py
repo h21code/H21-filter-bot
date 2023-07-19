c6aa71d819ebb7013511426e79d913ad9dad0289
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-19 12:37:33 +0530

    Update bardai.py

--- plugins/more/bardai.py
+++ plugins/more/bardai.py
@@ -8,6 +8,14 @@ API = "https://api.safone.me/bard?message="
 
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝗖𝗟𝗢𝗦𝗘", callback_data='close_data')]])
 
+def split_long_message(caption, max_length):
+    messages = []
+    while len(caption) > max_length:
+        messages.append(caption[:max_length])
+        caption = caption[max_length:]
+    messages.append(caption)
+    return messages
+
 def result(api_response, user, query):
     try:
         response_data = api_response['choices']
@@ -23,11 +31,13 @@ def result(api_response, user, query):
         # Limit the caption length to avoid MediaCaptionTooLong error
         max_caption_length = 1024  # Set the desired maximum length for the caption
         if len(result_str) > max_caption_length:
-            result_str = result_str[:max_caption_length]
+            messages = split_long_message(result_str, max_caption_length)
+        else:
+            messages = [result_str]
 
-        return result_str
+        return messages
     except Exception as error:
-        return str(error)
+        return [str(error)]
 
 @Client.on_message(filters.command("ai"))
 async def reply_info(client, message):
@@ -40,13 +50,15 @@ async def reply_info(client, message):
     # Get the user's username or first name if no username is available
     user = message.from_user.username or message.from_user.first_name
     
-    result_caption = result(api_response, user, query)
-    await message.reply_photo(
-        photo="https://telegra.ph/file/a4545775e137feda80612.jpg",
-        caption=result_caption,
-        reply_markup=BUTTONS,
-        quote=True
-    )
+    result_messages = result(api_response, user, query)
+    for idx, result_caption in enumerate(result_messages):
+        # Send each part of the caption as a separate message
+        await message.reply_photo(
+            photo="https://telegra.ph/file/a4545775e137feda80612.jpg",
+            caption=result_caption,
+            reply_markup=BUTTONS if idx == len(result_messages) - 1 else None,
+            quote=True if idx == 0 else False  # Quote the first message only
+        )
 
 @Client.on_callback_query(filters.regex('^close_data$'))
 async def close_data(client, callback_query):
