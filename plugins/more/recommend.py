df817831b1cc3458d9f21c01f36468b0c00ab12b
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-21 22:06:45 +0530

    Update recommend.py

--- plugins/more/recommend.py
+++ plugins/more/recommend.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from requests.utils import requote_uri
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -36,104 +37,66 @@ def get_series_recommendations(series_id):
     return []
 
 
-# Common function to handle the close button
-def handle_close_button(query):
-    query.message.delete()
-
-
-# Movie recommendation command handler
-@Client.on_message(filters.command("mrecommend"))
-def movie_recommendation(_, message):
-    query = message.text.strip()[15:]  # Remove '/movie_recommend' from the query
+# Movie and TV series recommendation command handler
+@Client.on_message(filters.command("recommend"))
+def media_recommendation(_, message):
+    query = message.text.strip()[10:]  # Remove '/recommend' from the query
     if query:
-        # Assuming query is the movie name, first we need to search for the movie to get its ID
-        url = f"{TMDB_API_URL}/search/movie"
-        params = {
-            "api_key": TMDB_API_KEY,
-            "query": query,
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            results = data.get('results', [])
-            if results:
-                movie_id = results[0]['id']  # Get the ID of the first movie in the search results
-                recommendations = get_movie_recommendations(movie_id)
+        # Get movie recommendations from TMDb API
+        movie_results = get_movie_recommendations(query)
 
-                # Create a list of buttons with recommended movie names
-                buttons = [
-                    [
-                        InlineKeyboardButton(movie['title'], callback_data=str(movie['id']))
-                    ]
-                    for movie in recommendations
+        # Get TV series recommendations from TMDb API
+        series_results = get_series_recommendations(query)
+
+        # Combine movie and TV series recommendations
+        media_results = movie_results + series_results
+
+        if media_results:
+            # Create a list of buttons with recommended movie/series names
+            buttons = [
+                [
+                    InlineKeyboardButton(media['title'] if 'title' in media else media['name'], callback_data=str(media['id']))
                 ]
+                for media in media_results
+            ]
 
-                # Create an "Close" button
-                close_button = InlineKeyboardButton("Close", callback_data="close")
-                keyboard = InlineKeyboardMarkup(buttons + [[close_button]])
+            # Add a "Close" button to the list of buttons
+            buttons.append([InlineKeyboardButton("Close", callback_data="close")])
 
-                if recommendations:
-                    message.reply_text("Choose a movie:", reply_markup=keyboard)
-                else:
-                    message.reply_text("Sorry, I couldn't find any movie recommendations for that query.")
-            else:
-                message.reply_text("Sorry, I couldn't find any movies for that query.")
+            # Create an InlineKeyboardMarkup with the buttons
+            keyboard = InlineKeyboardMarkup(buttons)
+
+            message.reply_text("Choose a movie/series:", reply_markup=keyboard)
         else:
-            message.reply_text("Sorry, an error occurred while fetching data.")
+            message.reply_text("Sorry, I couldn't find any movie or TV series recommendations for that query.")
     else:
-        message.reply_text("Please send the name of a movie along with the /movie_recommend tag to get recommendations!")
-
-
-# TV series recommendation command handler
-@Client.on_message(filters.command("srecommend"))
-def series_recommendation(_, message):
-    query = message.text.strip()[16:]  # Remove '/series_recommend' from the query
-    if query:
-        # Assuming query is the TV series name, first we need to search for the series to get its ID
-        url = f"{TMDB_API_URL}/search/tv"
-        params = {
-            "api_key": TMDB_API_KEY,
-            "query": query,
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            results = data.get('results', [])
-            if results:
-                series_id = results[0]['id']  # Get the ID of the first series in the search results
-                recommendations = get_series_recommendations(series_id)
-
-                # Create a list of buttons with recommended TV series names
-                buttons = [
-                    [
-                        InlineKeyboardButton(series['name'], callback_data=str(series['id']))
-                    ]
-                    for series in recommendations
-                ]
-
-                # Create an "Close" button
-                close_button = InlineKeyboardButton("Close", callback_data="close")
-                keyboard = InlineKeyboardMarkup(buttons + [[close_button]])
-
-                if recommendations:
-                    message.reply_text("Choose a TV series:", reply_markup=keyboard)
-                else:
-                    message.reply_text("Sorry, I couldn't find any TV series recommendations for that query.")
-            else:
-                message.reply_text("Sorry, I couldn't find any TV series for that query.")
-        else:
-            message.reply_text("Sorry, an error occurred while fetching data.")
-    else:
-        message.reply_text("Please send the name of a TV series along with the /series_recommend tag to get recommendations!")
+        message.reply_text("Please send the name of a movie or TV series along with the /recommend tag to get recommendations!")
 
 
 # Callback handler for button clicks
 @Client.on_callback_query()
 def button_click(_, query):
     if query.data == "close":
-        handle_close_button(query)
+        query.message.delete()
     else:
         media_id = int(query.data)
-        
+        url = f"{TMDB_API_URL}/movie/{media_id}"
+        params = {
+            "api_key": TMDB_API_KEY,
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            title = data.get('title')
+            overview = data.get('overview')
+            poster_path = data.get('poster_path')
 
+            if title and overview and poster_path:
+                poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
+                response_text = f"Title: {title}\n\n{overview}"
+                query.message.reply_photo(photo=poster_url, caption=response_text)
+            else:
+                query.message.reply_text("Sorry, couldn't fetch details for this movie/series.")
+        else:
+            query.message.reply_text("Sorry, an error occurred while fetching details for this movie/series.")
 
