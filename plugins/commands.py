f3635eeafa7d0218ba973543416e0213f3b51f02
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-23 10:48:20 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -667,81 +667,51 @@ async def save_template(client, message):
 
 @Client.on_message((filters.command(["request", "Request"]) | filters.regex("#request") | filters.regex("#Request")) & filters.group)
 async def requests(bot, message):
-    if REQST_CHANNEL is None or SUPPORT_CHAT_ID is None: return # Must add REQST_CHANNEL and SUPPORT_CHAT_ID to use this feature
-    if message.reply_to_message and SUPPORT_CHAT_ID == message.chat.id:
-        chat_id = message.chat.id
-        reporter = str(message.from_user.id)
-        mention = message.from_user.mention
-        success = True
-        content = message.reply_to_message.text
-        try:
-            if REQST_CHANNEL is not None:
-                btn = [[
-                        InlineKeyboardButton('View Request', url=f"{message.reply_to_message.link}"),
-                        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                      ]]
-                reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"<b>Reporter : {mention} ({reporter})\n\nMessage : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
-                success = True
-            elif len(content) >= 3:
-                for admin in ADMINS:
-                    btn = [[
-                        InlineKeyboardButton('View Request', url=f"{message.reply_to_message.link}"),
-                        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                      ]]
-                    reported_post = await bot.send_message(chat_id=admin, text=f"<b>Reporter : {mention} ({reporter})\n\nMessage : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
-                    success = True
-            else:
-                if len(content) < 3:
-                    await message.reply_text("<b>You must type about your request [Minimum 3 Characters]. Requests can't be empty.</b>")
-            if len(content) < 3:
-                success = False
-        except Exception as e:
-            await message.reply_text(f"Error: {e}")
-            pass
-        
-    elif SUPPORT_CHAT_ID == message.chat.id:
-        chat_id = message.chat.id
-        reporter = str(message.from_user.id)
-        mention = message.from_user.mention
-        success = True
-        content = message.text
-        keywords = ["#request", "/request", "#Request", "/Request"]
-        for keyword in keywords:
-            if keyword in content:
-                content = content.replace(keyword, "")
-        try:
-            if REQST_CHANNEL is not None and len(content) >= 3:
-                btn = [[
-                        InlineKeyboardButton('View Request', url=f"{message.link}"),
-                        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                      ]]
-                reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"<b>Reporter : {mention} ({reporter})\n\nMessage : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
-                success = True
-            elif len(content) >= 3:
-                for admin in ADMINS:
-                    btn = [[
-                        InlineKeyboardButton('View Request', url=f"{message.link}"),
-                        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                      ]]
-                    reported_post = await bot.send_message(chat_id=admin, text=f"<b>Reporter : {mention} ({reporter})\n\nMessage : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
-                    success = True
-            else:
-                if len(content) < 3:
-                    await message.reply_text("<b>You must type about your request [Minimum 3 Characters]. Requests can't be empty.</b>")
-            if len(content) < 3:
-                success = False
-        except Exception as e:
-            await message.reply_text(f"Error: {e}")
-            pass
+    try:
+        if REQST_CHANNEL is None or SUPPORT_CHAT_ID is None:
+            return  # Must add REQST_CHANNEL and SUPPORT_CHAT_ID to use this feature
 
-    else:
-        success = False
-    
-    if success:
+        if message.from_user is None:
+            await message.reply_text("<b>Error: Unable to determine the user information.</b>")
+            return
+
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        success = True
+        content = ""
+
+        if message.reply_to_message and SUPPORT_CHAT_ID == chat_id:
+            content = message.reply_to_message.text
+        else:
+            content = message.text
+            keywords = ["#request", "/request", "#Request", "/Request"]
+            for keyword in keywords:
+                if keyword in content:
+                    content = content.replace(keyword, "")
+
+        if len(content) < 3:
+            await message.reply_text("<b>You must type about your request [Minimum 3 Characters]. Requests can't be empty.</b>")
+            return
+
         btn = [[
-                InlineKeyboardButton('View Request', url=f"{reported_post.link}")
-              ]]
+            InlineKeyboardButton('View Request', url=f"{message.link}"),
+            InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
+        ]]
+
+        if REQST_CHANNEL is not None:
+            reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"<b>Reporter: {mention} ({reporter})\n\nMessage: {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
+        else:
+            for admin in ADMINS:
+                reported_post = await bot.send_message(chat_id=admin, text=f"<b>Reporter: {mention} ({reporter})\n\nMessage: {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
+
+    except Exception as e:
+        await message.reply_text(f"Error: {e}")
+        success = False
+
+    if success:
         await message.reply_text("<b>Your request has been added! Please wait for some time.</b>", reply_markup=InlineKeyboardMarkup(btn))
+
 
         
 @Client.on_message(filters.command("send") & filters.user(ADMINS))
