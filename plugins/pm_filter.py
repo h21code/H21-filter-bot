de3f07c546ca25a88a69b2d24aa8d66686970744
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-07-23 14:54:56 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2669,7 +2669,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = script.NOR_TXT.format(message.from_user.mention, search, total_results, message.chat.title)
+        cap = script.NOR_TXT.format(message.from_user.mention, total_results, search, message.chat.title)
     if imdb and imdb.get('poster'):
         try:
             if message.chat.id == SUPPORT_CHAT_ID:
